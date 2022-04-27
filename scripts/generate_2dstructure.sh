c84ade211530f798f1aef5ee489f89d5d8656fbd
kkyamada <kkyamada1111@gmail.com>
2022-04-27 15:52:16 +0900

    updated scripts

--- scripts/generate_2dstructure.sh
+++ scripts/generate_2dstructure.sh
@@ -3,12 +3,13 @@
 PYTHON_PATH=../examples/create_secondary_structure.py
 ORIG_PATH=../sample_dataset/
 PATH2=nontraining_sample_finetune/hg38/
-PATH_TO_LINEARPARTITION="ENTER THE PATH YOU SAVED LINEARPARTITION"
 NUM_EACH=1000
 
 RBP=$1
 echo $RBP
 # RBP=TIAL1
+PATH_TO_LINEARPARTITION=$2
+echo $PATH_TO_LINEARPARTITION
 
 python3 $PYTHON_PATH --rbp $RBP --data_dir $ORIG_PATH --path_to_linearpartition $PATH_TO_LINEARPARTITION --data_dir_suffix $PATH2 --num_each $NUM_EACH
 
